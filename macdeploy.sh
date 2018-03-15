de112363db60904581e4ddcb1129e86f5410df46
Christian Mollekopf <mollekopf@kolabsys.com>
2018-03-15 13:55:10 -0700

    Deploy script updates

--- macdeploy.sh
+++ macdeploy.sh
@@ -1,26 +1,9 @@
 
-macdeployqt $PWD/Applications/KDE/kube.app
+APP=Applications/KDE/kube.app
+macdeployqt $PWD/$APP -always-overwrite -qmldir=$PWD/lib/qml
 
-#    rm -rf Applications/KDE/kube.app/Contents/Frameworks/Qt3DCore.framework
-#    rm -rf Applications/KDE/kube.app/Contents/Frameworks/Qt3DRender.framework
-#    rm -rf Applications/KDE/kube.app/Contents/Frameworks/QtLocation.framework
-#    rm -rf Applications/KDE/kube.app/Contents/Frameworks/QtMultimedia.framework
-#    rm -rf Applications/KDE/kube.app/Contents/Frameworks/QtMultimediaWidgets.framework
-#    rm -rf Applications/KDE/kube.app/Contents/Frameworks/QtPositioning.framework
-#    rm -rf Applications/KDE/kube.app/Contents/Frameworks/QtSerialBus.framework
-#    rm -rf Applications/KDE/kube.app/Contents/Frameworks/QtSerialPort.framework
-#    rm -rf Applications/KDE/kube.app/Contents/Frameworks/QtWebChannel.framework
-#    rm -rf Applications/KDE/kube.app/Contents/Frameworks/QtWebEngineCore.framework
-#    rm -rf Applications/KDE/kube.app/Contents/Frameworks/QtWebEngineWidgets.framework
-#    rm -rf Applications/KDE/kube.app/Contents/PlugIns/qmltooling
-#    rm -rf Applications/KDE/kube.app/Contents/PlugIns/position
-#    rm -rf Applications/KDE/kube.app/Contents/PlugIns/playlistformats
-#    rm -rf Applications/KDE/kube.app/Contents/PlugIns/mediaservice
-#    rm -rf Applications/KDE/kube.app/Contents/PlugIns/geoservices
-#    rm -rf Applications/KDE/kube.app/Contents/PlugIns/canbus
-#    rm -rf Applications/KDE/kube.app/Contents/PlugIns/sceneparsers
-#    rm -rf Applications/KDE/kube.app/Contents/PlugIns/sensorgestures
-#    rm -rf Applications/KDE/kube.app/Contents/PlugIns/sensors
+#Install qml files
+cp -R $PWD/lib/qml/org $PWD/$APP/Contents/Resources/qml/
 
 # create the final disk image
 macdeployqt Applications/KDE/kube.app -dmg
