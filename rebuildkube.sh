64835f80492fd578ba919ecfe8eb01d40107838a
Christian Mollekopf <mollekopf@kolabsys.com>
2018-03-28 06:30:53 -0700

    rebuildkube.sh

---
+++ rebuildkube.sh
@@ -0,0 +1,15 @@
+set -e
+
+export CMAKE_LIBRARY_PATH=/usr/local/Cellar/gettext/0.19.8.1/lib:/usr/local/Cellar/readline/7.0.3_1/lib
+export CMAKE_INCLUDE_PATH=/usr/local/Cellar/gettext/0.19.8.1/include:/usr/local/Cellar/readline/7.0.3_1/include
+export PATH=/usr/local/Cellar/gettext/0.19.8.1/bin:$PATH
+export MACOSX_DEPLOYMENT_TARGET=10.9.0 #Minimum version to find the required type_traits header
+export SDKROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/
+
+. craft/craftenv.sh
+
+craft kde/pim/kcalcore
+craft --install-deps --fetch --unpack --compile --install --qmerge extragear/sink
+craft --install-deps --fetch --unpack --compile --install --qmerge extragear/kube
+
+./macdeploy.sh
